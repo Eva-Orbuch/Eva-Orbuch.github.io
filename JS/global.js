daa1d7838f0f674c4891b6798257fd55755c4844
Professor.Goo <bpgrandy@gmail.com>
2021-01-29 22:06:17 -0800

    move bootstrap js to global

--- JS/global.js
+++ JS/global.js
@@ -2,4 +2,4 @@
 function includeJS(incFile) {document.write('<script type="text/javascript" src="' + incFile + '"></script>'); }
 
 includeJS('https://code.jquery.com/jquery-latest.js');
-
+includeJS('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js');
