7ffcbb5d9941940e400911de1fa39c23c08ca56c
Professor.Goo <bpgrandy@gmail.com>
2021-02-25 20:25:03 -0800

    parallax

--- JS/global.js
+++ JS/global.js
@@ -3,3 +3,4 @@ function includeJS(incFile) {document.write('<script type="text/javascript" src=
 
 includeJS('https://code.jquery.com/jquery-latest.js');
 includeJS('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js');
+includeJS('../JS/parallax.js');
