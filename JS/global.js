83f6490a894ea3a0126718bd9bb0ebbb6f89eb83
Professor.Goo <bpgrandy@gmail.com>
2021-05-19 12:47:12 -0400

    bootstrap 5

--- JS/global.js
+++ JS/global.js
@@ -2,6 +2,8 @@
 function includeJS(incFile) {document.write('<script type="text/javascript" src="' + incFile + '"></script>'); }
 
 includeJS('https://code.jquery.com/jquery-latest.js');
-includeJS('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js');
-includeJS('../JS/parallax.js');
+//includeJS('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js');
+//includeJS('../JS/parallax.js');
 
+// JavaScript Bundle with Popper
+includeJS('https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js');
